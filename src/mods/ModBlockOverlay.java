27ee77dd86e8cba20b7048255d12747e57a1e8a5
lllllIIIllIlllIlll <10230075@kamagaku.ed.jp>
2025-10-02 07:50:59 +0900

    Add files via upload

---
+++ src/mods/ModBlockOverlay.java
@@ -0,0 +1,10 @@
+package mods;
+
+// Block highlight overlay
+public class ModBlockOverlay extends Mod {
+    public ModBlockOverlay() { super("Block Overlay"); }
+    @Override
+    public void onRender2D(float partialTicks) {
+        // TODO: Draw outline or overlay on looked-at block.
+    }
+}

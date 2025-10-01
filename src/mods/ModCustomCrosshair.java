00854c6f96e9d4cda338eb5a5cb496d7bce08d83
lllllIIIllIlllIlll <10230075@kamagaku.ed.jp>
2025-10-02 07:47:14 +0900

    Add files via upload

---
+++ src/mods/ModCustomCrosshair.java
@@ -0,0 +1,10 @@
+package mods;
+
+// Custom crosshair renderer
+public class ModCustomCrosshair extends Mod {
+    public ModCustomCrosshair() { super("Custom Crosshair"); }
+    @Override
+    public void onRender2D(float partialTicks) {
+        // TODO: Draw custom crosshair at screen center.
+    }
+}

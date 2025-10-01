aa45d79b3a5c36b9ae89b84c704ae89e7e7ff0f0
lllllIIIllIlllIlll <10230075@kamagaku.ed.jp>
2025-10-02 07:45:27 +0900

    Add files via upload

---
+++ src/mods/ModToggleSprint.java
@@ -0,0 +1,10 @@
+package mods;
+
+// Allows toggling sprint/sneak with one keypress
+public class ModToggleSprint extends Mod {
+    public ModToggleSprint() { super("Toggle Sprint/Sneak"); }
+    @Override
+    public void onTick() {
+        // TODO: Implement toggle logic for sprint/sneak keys.
+    }
+}

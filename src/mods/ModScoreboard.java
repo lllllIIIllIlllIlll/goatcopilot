435eb747bb58698924f52866b1c4d5b0dd151acc
lllllIIIllIlllIlll <10230075@kamagaku.ed.jp>
2025-10-02 07:50:14 +0900

    Add files via upload

---
+++ src/mods/ModScoreboard.java
@@ -0,0 +1,10 @@
+package mods;
+
+// Customize side scoreboard appearance
+public class ModScoreboard extends Mod {
+    public ModScoreboard() { super("Scoreboard Mod"); }
+    @Override
+    public void onRender2D(float partialTicks) {
+        // TODO: Draw or modify the scoreboard display.
+    }
+}

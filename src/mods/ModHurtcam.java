8c1b27681533beaf3070373d3ec4d339312b5ab4
lllllIIIllIlllIlll <10230075@kamagaku.ed.jp>
2025-10-02 07:48:00 +0900

    Add files via upload

---
+++ src/mods/ModHurtcam.java
@@ -0,0 +1,10 @@
+package mods;
+
+// Change or disable hurt camera shake
+public class ModHurtcam extends Mod {
+    public ModHurtcam() { super("Hurtcam Customizer"); }
+    @Override
+    public void onTick() {
+        // TODO: Hook into camera shake logic and modify/disable as needed.
+    }
+}

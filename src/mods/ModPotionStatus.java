7a93a5e01ed172f4b070e707a9de6d70adf0034c
lllllIIIllIlllIlll <10230075@kamagaku.ed.jp>
2025-10-02 07:43:59 +0900

    Add files via upload

---
+++ src/mods/ModPotionStatus.java
@@ -0,0 +1,12 @@
+package mods;
+
+import net.minecraft.client.Minecraft;
+
+// Shows active potion effects on HUD
+public class ModPotionStatus extends Mod {
+    public ModPotionStatus() { super("Potion Status"); }
+    @Override
+    public void onRender2D(float partialTicks) {
+        // TODO: Render potion effect icons, duration, and amplifier.
+    }
+}
